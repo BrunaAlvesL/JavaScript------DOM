35120e3684fb037f80455778b5d99518652973a4
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-05-30 20:47:19 -0300

    commit 6

--- 04-obtendo-elementos/index.js
+++ 04-obtendo-elementos/index.js
@@ -1,4 +1,10 @@
-function (){
+function show(){
     const contactList = document.getElementById('contact-list')
     console.log(contactList)
 }
+
+const listElements = document.getElementsByTagName('li')
+console.log(listElements)
+
+const contacInputs = document.getElementsByClassName('contact-input')
+console.log(contacInputs)
