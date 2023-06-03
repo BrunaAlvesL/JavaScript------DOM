afe229763b198c556f7ab72ae44a1b0a74d53406
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-06-03 11:48:08 -0300

    03/06commit

--- 04-obtendo-elementos/index.js
+++ 04-obtendo-elementos/index.js
@@ -8,3 +8,12 @@ console.log(listElements)
 
 const contacInputs = document.getElementsByClassName('contact-input')
 console.log(contacInputs)
+
+const contacts = document.querySelectorAll('#contact-list > li > label')
+console.log(contacts)
+
+const contact1 = document.getElementsByName('contact1')
+console.log(contact1)
+
+const firstContact = document.querySelector('#contact-list > li > label')
+console.log(firstContact)
